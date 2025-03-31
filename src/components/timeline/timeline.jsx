75cbeb455ba77a76aa57c4cb11c9d8b37a4f98c5
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-03-31 15:49:28 -0400

    Removed extra bullet points to limit wording.

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -9,19 +9,34 @@ function Timeline() {
     </header>
     <section className="timeline-container">
       <div className="timeline">
-      <article className="timeline-item">
+        <article className="timeline-item">
           <div className="timeline-content">
-            <h3>Outlier AI | AI Prompt Engineer Intern</h3>
-            <p className="timeline-time">Dec 2024 - Feb 2025</p>
+            <h3>Cross Screen Media | Software Engineer Intern</h3>
+            <p className="timeline-time">May 2025 - September 2025</p>
             <ul>
               <li>
-                Developed Python scripts to improve AI image recognition capabilities, focusing on improving coding-related image analysis.
+                Building and managing full-stack web applications.
+              </li>
+            </ul>
+            <div className="timeline-tech">
+              <span>Python/DJango</span>
+              <span>React.js</span>
+              <span>JavaScript</span>
+            </div>
+          </div>
+        </article>
+        <article className="timeline-item">
+          <div className="timeline-content">
+            <h3>Outlier AI | AI Prompt Engineer Intern</h3>
+            <p className="timeline-time">December 2024 - February 2025</p>
+            <ul>
+              <li>
+                Developed scripts to improve AI image recognition capabilities.
               </li>
             </ul>
             <div className="timeline-tech">
               <span>Python</span>
               <span>AI/ML</span>
-              <span>Image Processing</span>
             </div>
           </div>
         </article>
@@ -30,14 +45,8 @@ function Timeline() {
           <div className="timeline-content">
             <h3>Freelance Developer</h3>
             <p className="timeline-time">July 2024 - Present</p>
-            <p className="timeline-description">
-              Coding side projects to further my software development skills and learn new technologies.
-            </p>
             <ul>
-              <li>Leading the development of StockSim, a full-stack Stock Market simulator.</li>
-              <li>Developed MorningFlow to reduce the amount of time it takes to get morning updates.</li>
-              <li>Developed a stock market game to create a risk free and fun way to start learning about the market.</li>
-              <li>Developed this portfolio in React.js.</li>
+              <li>Coding side projects to learn new technologies and gain experience.</li>
             </ul>
             <div className="timeline-tech">
               <span>Python/DJango</span>
@@ -52,11 +61,10 @@ function Timeline() {
         <article className="timeline-item">
           <div className="timeline-content">
             <h3>LandMarksCM | Web Development Intern</h3>
-            <p className="timeline-time">Jan 2024 - Mar 2024</p>
+            <p className="timeline-time">January 2024 - March 2024</p>
             <ul>
-              <li>Designed and managed a responsive company website using WordPress and JavaScript.</li>
-              <li>Implemented a captcha system that eliminated 96% of spam emails received by the company, significantly reducing manual email filtering.</li>
-              <li>Assisted the company in adopting AI to automate repetitive tasks and improve efficiency.</li>
+              <li>Designed and developed LandmarksCM website.</li>
+              <li>Assisted the company in adopting AI for repetitive tasks.</li>
             </ul>
             <div className="timeline-tech">
             <span>WordPress</span>
@@ -68,14 +76,10 @@ function Timeline() {
         <article className="timeline-item">
           <div className="timeline-content">
             <h3>James Madison University | Computer Science</h3>
-            <p className="timeline-time">Aug 2023 - Present</p>
-            <p className="timeline-description">
-              Computer Science student focusing on software development.
-            </p>
+            <p className="timeline-time">August 2023 - Present</p>
             <ul>
-              <li>Collaborated with other students to create a digital cookbook application.</li>
-              <li>Competitor in the JMU Cyber Defense Club.</li>
-              <li>Built various school projects in JavaScript, Java, Python, and C.</li>
+              <li>Computer Science student at James Madison University focusing on full-stack software engineering and machine learning.
+            </li>
             </ul>
             <div className="timeline-tech">
               <span>Java</span>
