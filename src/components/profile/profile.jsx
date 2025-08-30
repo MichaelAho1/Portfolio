bcf5a3a467053a236edf7ddb994eae6db4434820
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-08-30 11:41:44 -0400

    .

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -27,6 +27,7 @@ function Profile() {
           <h1 id="name">Michael Aho</h1>
           <label id="description">
             CS @ JMU<br />
+            Prev SWE Intern @ Cross Screen Media<br />
           </label>
           <div>
             <h2 id="contact-header">Contact info</h2>
