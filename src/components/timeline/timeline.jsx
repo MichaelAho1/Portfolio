5e121a74f31acb207d64f3c1f631bcf88af73947
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-08-30 11:51:09 -0400

    .

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -13,12 +13,6 @@ function Timeline() {
           <div className="timeline-content">
             <h3>Cross Screen Media | Software Engineering Intern</h3>
             <p className="timeline-time">May 2025 - August 2025</p>
-            <ul>
-              <li>Developed full-stack features for an analytics platform using React, TypeScript, and Django, including a filtering tool that supports 300+ audience attributes, improving audience targeting for 1,500+ advertising campaigns</li>
-              <li>Migrated 15 TypeScript end-to-end test suites from Cypress to RainforestQA, enabling parallel test execution, reducing total test runtime by 68% (from 25 to 8 minutes)</li>
-              <li>Optimized attribute load speeds with React, using Redux caching to reduce redundant API requests</li>
-              <li>Engineered a data pipeline with Python and SQL, resolving data handling errors for 75+ audience attributes</li>
-            </ul>
             <div className="timeline-tech">
               <span>React.js</span>
               <span>TypeScript</span>
@@ -32,10 +26,6 @@ function Timeline() {
           <div className="timeline-content">
             <h3>LandMarksCM | Web Development Intern</h3>
             <p className="timeline-time">January 2024 - April 2024</p>
-            <ul>
-              <li>Built a client-facing website with JavaScript and CSS, integrating Google reCaptcha to reduce spam emails by 96%</li>
-              <li>Developed an automation tool with Python, using OpenAI’s GPT-4 API to standardize inconsistent column headers and Pandas to merge audience data across multiple spreadsheets, automating 4 hours of manual work per week</li>
-            </ul>
             <div className="timeline-tech">
             <span>Python</span>
             <span>JavaScript</span>
